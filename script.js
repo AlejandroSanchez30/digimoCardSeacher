46e1286431d218825560f5c2aa2b8a3c2498ada0
Alejandro Sanchez <arehandoro23@gmail.com>
2022-07-22 16:19:19 -0700

    Card type filters added

--- script.js
+++ script.js
@@ -6,34 +6,115 @@ const cardContainer = document.getElementById('card-container');
 const btnSearch = document.getElementById('searchButton');
 const textField = document.getElementById('searchField');
 
-//radiobuttons
+//Filter cards by color
 const colorFilters = document.querySelectorAll('.color-filter');
 colorFilters.forEach(color => {
     color.addEventListener('click', async function (event) {
-        if(color.checked)
-        {
-            colorFilters.forEach(filter =>{
-                if(filter.checked){
-                    if(filter.id != color.id)
-                    {
-                        filter.checked = false;
-                    }
+        let parameters = '';
+        if(color.checked){
+            uncheckOthersFilters(colorFilters, color.id);
+            if(textField.value.length != 0)
+            {
+                parameters += `n=${textField.value}`;
+                const activeType = checkActiveFilter(typeFilters);
+                if(activeType != null){
+                    parameters +=`&type=${activeType}`;
                 }
-            });
-            var data = await getData('color=' + color.id + '&');
+                parameters +=`&color=${color.id}`;
+            }
+            else{
+                const activeType = checkActiveFilter(typeFilters);
+                console.log(activeType);
+                if(activeType != null){
+                    
+                    parameters +=`&type=${activeType}&color=${color.id}`;
+                }
+                else{
+                    parameters +=`color=${color.id}`;
+                }
+                
+            }
+        }
             
+            const data = await getData(parameters);
+            showCards(data);
+    
+})});
 
+
+//Filter card by type
+const typeFilters = document.querySelectorAll('.type-filter');
+typeFilters.forEach(type => {
+    type.addEventListener('click', async function () {
+        let parameters = '';
+        if(type.checked){
+            uncheckOthersFilters(typeFilters, type.id);
+            if(textField.value.length != 0)
+            {
+                parameters += `n=${textField.value}`;
+                
+                const colorActive = checkActiveFilter(colorFilters);
+                if(colorActive != null)
+                {
+                    parameters += `&color=${colorActive}&type=${type.id}`;
+                }
+                else{
+                    parameters +=`&type${type.id}`;
+                }
+            }
+            else{
+
+                const colorActive = checkActiveFilter(colorFilters);
+                
+                if(colorActive != null)
+                {
+                    parameters += `color=${colorActive}&type=${type.id}`;
+                }
+                else{
+                    parameters += `type=${type.id}`
+                }
+            }
         }
-        else{
-            var data = await getData();
-        }
+        const data = await getData(parameters);
         showCards(data);
     });
     
 });
 
+function initializeFilters(filtersContainer)
+{
+    filtersContainer.forEach(filter => {
+        filter.checked = false;
+    });
+}
+
+function checkActiveFilter(filtersContainer)
+{
+    let id = null;
+    filtersContainer.forEach(filter => {
+        if(filter.checked)
+        {
+            id =  filter.id;
+        }
+    });
+    return id;
+    
+}
+
+function uncheckOthersFilters(filtersContainer, idActiveFilter)
+{
+    filtersContainer.forEach(filter => {
+        if(filter.id != idActiveFilter){
+            filter.checked = false;
+        }
+    });
+}
+
+
 
 btnSearch.addEventListener('click', async () => {
+    initializeFilters(colorFilters);
+    initializeFilters(typeFilters);
     const data = await getData('n=' + textField.value + '&');
     showCards(data);
 }
@@ -42,6 +123,8 @@ btnSearch.addEventListener('click', async () => {
 textField.addEventListener('keydown', async function(event) {
     if(event.code == 'Enter')
     {
+        initializeFilters(typeFilters);
+        initializeFilters(colorFilters);
         const data = await getData('n=' + textField.value + '&');
         showCards(data);
     }
@@ -50,24 +133,29 @@ textField.addEventListener('keydown', async function(event) {
 
 
 
-async function getData(urlParameters = ''){
-    const response = await fetch(`https://digimoncard.io/api-public/search.php?${urlParameters}sort=card_number&series=Digimon Card Game`);
-    const data = await response.json();
-    return data; 
+async function getData(urlParameters = "") {
+  if (urlParameters.length != 0) {
+    urlParameters += "&";
+  }
+  const response = await fetch(
+    `https://digimoncard.io/api-public/search.php?${urlParameters}sort=card_number&series=Digimon Card Game`
+  );
+  const data = await response.json();
+  return data;
 }
 
-function showCards(cards = ''){
-    cardContainer.innerHTML = '';    
-    cards.forEach(card => {
-        const cardDiv = document.createElement('div');
-        cardDiv.classList.add('card');
-         const cardImage = document.createElement('img');
-         cardImage.setAttribute('src', card.image_url);
-         cardImage.setAttribute('alt', card.name);
-        
-         cardDiv.appendChild(cardImage);
-         cardContainer.appendChild(cardDiv);
-    });
+function showCards(cards = "") {
+  cardContainer.innerHTML = "";
+  cards.forEach((card) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("src", card.image_url);
+    cardImage.setAttribute("alt", card.name);
+
+    cardDiv.appendChild(cardImage);
+    cardContainer.appendChild(cardDiv);
+  });
 }
 
 async function initialize(){
