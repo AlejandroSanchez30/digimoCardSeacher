be71966dca12775b6e4785a362f43dec13487b80
Alejandro Sanchez <arehandoro23@gmail.com>
2022-07-22 12:22:56 -0700

    Color filters added

--- script.js
+++ script.js
@@ -6,12 +6,49 @@ const cardContainer = document.getElementById('card-container');
 const btnSearch = document.getElementById('searchButton');
 const textField = document.getElementById('searchField');
 
+//radiobuttons
+const colorFilters = document.querySelectorAll('.color-filter');
+colorFilters.forEach(color => {
+    color.addEventListener('click', async function (event) {
+        if(color.checked)
+        {
+            colorFilters.forEach(filter =>{
+                if(filter.checked){
+                    if(filter.id != color.id)
+                    {
+                        filter.checked = false;
+                    }
+                }
+            });
+            var data = await getData('color=' + color.id + '&');
+            
+
+        }
+        else{
+            var data = await getData();
+        }
+        showCards(data);
+    });
+    
+});
+
 
 btnSearch.addEventListener('click', async () => {
     const data = await getData('n=' + textField.value + '&');
     showCards(data);
 }
 );
+
+textField.addEventListener('keydown', async function(event) {
+    if(event.code == 'Enter')
+    {
+        const data = await getData('n=' + textField.value + '&');
+        showCards(data);
+    }
+});
+
+
+
 
 async function getData(urlParameters = ''){
     const response = await fetch(`https://digimoncard.io/api-public/search.php?${urlParameters}sort=card_number&series=Digimon Card Game`);
